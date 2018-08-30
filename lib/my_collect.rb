fcbc34714a88c943ad29c8bcff375077cba99518
Amalie S <amaliesmidth@gmail.com>
2018-08-30 16:11:56 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,10 @@
 def my_collect(collection)
   i = 0
+  newcollection = []
   while i < collection.length 
-    yield(collection[i])
+    newcollection << yield(collection[i])
     i += 1
   end
+  newcollection
 end  
 
